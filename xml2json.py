1ebcd6c4e9f66bc4d28c80df8952698912b7d352
sycured <60801403+sycured@users.noreply.github.com>
2020-03-12 21:02:49 -0500

    migrate to specific import

--- xml2json.py
+++ xml2json.py
@@ -1,19 +1,19 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
-import sys
-import argparse
+from json import dump
+from sys import stdin, stdout
+from argparse import ArgumentParser, FileType
 
-parser = argparse.ArgumentParser(description='Convert an XML file to JSON.')
+parser = ArgumentParser(description='Convert an XML file to JSON.')
 parser.add_argument('--unsafe', action='store_true',
                     help='do not use defusedxml: only for known-safe XML!')
-parser.add_argument('infile', nargs='?', type=argparse.FileType('rt'),
-                    default=sys.stdin)
+parser.add_argument('infile', nargs='?', type=FileType('rt'),
+                    default=stdin)
 
 
 def xml_element_to_dict(elem):
-    "Convert XML Element to a simple dict"
+    """Convert XML Element to a simple dict"""
     inner = dict(elem.attrib)
     children = list(map(xml_element_to_dict, list(elem)))
     text = elem.text and elem.text.strip()
@@ -26,16 +26,16 @@ def xml_element_to_dict(elem):
 
 
 def main(args):
-    "Dump JSON-from-parsed-XML to stdout"
+    """Dump JSON-from-parsed-XML to stdout"""
     if args.unsafe:
-        import xml.etree.cElementTree as ElementTree
+        from xml.etree.cElementTree import parse
     else:
-        import defusedxml.cElementTree as ElementTree
+        from defusedxml.cElementTree import parse
 
-    xml_parser = ElementTree.parse(args.infile)
+    xml_parser = parse(args.infile)
     root = xml_parser.getroot()
 
-    json.dump(xml_element_to_dict(root), sys.stdout, indent=2)
+    dump(xml_element_to_dict(root), stdout, indent=2)
     print()
 
 
